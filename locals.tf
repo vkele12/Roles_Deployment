03aadf5520d1b61432711f98e419cd106aff4ccf
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-12 14:56:07 +0530

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,7 @@
+locals {
+  lpa_roles_prefix           = "pltx"
+  devops_support_role        = "aws-saas-platform-devops-admin"
+  role_deployment_account_id = "003639144106"
+  deploying_role             = "aws-saas-platform-devops-admin"
+  lpa_organization_ou        = "ou-9d1y-91f03w0t"
+}
