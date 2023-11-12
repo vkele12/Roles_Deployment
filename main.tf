bff71e97f83b677b4b06cb09eb32a99731607b52
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-12 15:12:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "products-management-deployment-role-consume" {
-  source                     = "../../"
-  deploying_role             = local.deploying_role
-  role_deployment_account_id = local.role_deployment_account_id
-  lpa_organization_ou        = local.lpa_organization_ou
-  providers = {
-    aws.main = aws.main
-  }
-}
+# module "products-management-deployment-role-consume" {
+#  source                     = "../../"
+#  deploying_role             = local.deploying_role
+#  role_deployment_account_id = local.role_deployment_account_id
+#  lpa_organization_ou        = local.lpa_organization_ou
+#  providers = {
+#    aws.main = aws.main
+#  }
+#}
