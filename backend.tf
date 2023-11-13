9c0d89475463a6d4dc57d1b540d6237ff9d93da1
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-13 16:14:59 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-state-bucket-444445230229"
    # key     = "deployment_roles/${var.role_deployment_account_id}/user-management-deployment-role/terraform.tfstate"
-     key     = var.backend_key
+   #  key     = var.backend_key
     encrypt = true
     region  = "ap-south-1"
   }
