9378592afe764dc3da5cea3b6fb8884a201f828a
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-13 16:38:45 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 ## output will contain id of the deployed role
 output "deployed_role_id" {
-  value = aws_iam_role.var.lpa_output_file_role_change.id
+  value = aws_iam_role.${var.lpa_output_file_role_change}.id
 }
 
 output "deployed_role_arn" {
-  value = aws_iam_role.var.lpa_output_file_role_change.arn
+  value = aws_iam_role.${var.lpa_output_file_role_change}.arn
 }
