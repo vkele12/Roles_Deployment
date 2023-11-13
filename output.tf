bca6270c335e43b4392b98a78f14ef041321a693
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-13 19:27:20 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "deployed_role_id" {
-  value = aws_iam_role[local.lpa_output_file_role_change].id
+  value = aws_iam_role[var.lpa_output_file_role_change].id
 }
 
 output "deployed_role_arn" {
-  value = aws_iam_role[local.lpa_output_file_role_change].arn
+  value = aws_iam_role[var.lpa_output_file_role_change].arn
 }
