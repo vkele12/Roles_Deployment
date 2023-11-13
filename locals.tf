c2dbddd3a4319ff345d232c788482c186f158ff1
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-13 19:53:56 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -4,5 +4,5 @@ locals {
   role_deployment_account_id = var.role_deployment_account_id
   deploying_role             = "aws-saas-platform-devops-admin"
   lpa_organization_ou        = var.lpa_organization_ou
-  lpa_output_file_role_change = var.lpa_output_file_role_change
+ # lpa_output_file_role_change = var.lpa_output_file_role_change
 }
