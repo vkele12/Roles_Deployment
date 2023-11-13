04adb425e5a7cfef2da93db8d578c4049690fb7b
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-13 16:35:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-bucket-444445230229"
+    bucket  = "terraform-state-bucket-1-444445230229"
    # key     = "deployment_roles/${var.role_deployment_account_id}/user-management-deployment-role/terraform.tfstate"
    #  key     = var.backend_key
     encrypt = true
