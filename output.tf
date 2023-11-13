2c9015040d448479368dcfc386e271ac307d1120
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-13 22:25:03 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "debug_info" {
-  value = local.lpa_output_file_role_change
+  value = {
+    lpa_output_file_role_change = var.lpa_output_file_role_change
+    aws_iam_roles              = aws_iam_role.names # Adjust this based on your actual resource structure
+  }
 }
 
 output "deployed_role_id" {
-  value = aws_iam_role[var.lpa_output_file_role_change].id
+  value = aws_iam_role[var.lpa_output_file_role_change] ? aws_iam_role[var.lpa_output_file_role_change].id : null
 }
 
 output "deployed_role_arn" {
-  value = aws_iam_role[var.lpa_output_file_role_change].arn
+  value = aws_iam_role[var.lpa_output_file_role_change] ? aws_iam_role[var.lpa_output_file_role_change].arn : null
 }
